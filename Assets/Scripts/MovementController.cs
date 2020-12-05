ec545655cb3411e4621d965e4a924c46566de6a0
Thomas <thomasnelson2123@gmail.com>
2020-12-04 22:28:10 -0800

    First commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public PlayerController controller;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 400f;           // the amount of force the player jumps with
+    [SerializeField] private Transform groundCheck;     // a transform that will be used to detect if the player is grounded
+    [SerializeField] private LayerMask whatIsGround;    // layer mask that defines what ground is
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = 0.05f;	// How much to smooth out the movement
+
+    const float GroundCheckRadius = 0.25f;               // the radius for checking if the player is grounded
+    private bool isGrounded;                            // boolean, if true then player is grounded, if false player is in the air
+    private bool facingRight = true;                    // For determining which way the player is currently facing.
+
+    private Rigidbody2D rb;
+    private Vector3 v = Vector3.zero;
+
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+        {
+            OnLandEvent = new UnityEvent();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        //The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, GroundCheckRadius, whatIsGround);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
+    }
+
+    public void Move(float move, bool jump)
+    {
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+        // And then smoothing it out and applying it to the character
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref v, movementSmoothing);
+
+        if (move < 0 && facingRight)
+        {
+            // flip left
+
+            Flip();
+        }
+        if (move > 0 && !facingRight)
+        {
+            //flip right
+            Flip();
+        }
+
+        // If the player should jump...
+        if (isGrounded && jump)
+        {
+            // Add a vertical force to the player.
+            isGrounded = false;
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
